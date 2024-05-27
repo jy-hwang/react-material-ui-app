32e06b0376c7475ed12d40bf91d95b414dec221b
jy-hwang <stratojy@gmail.com>
2024-05-28 02:21:12 +0900

    Cards 형식의 본문작업
    1. 화면 가운데 카드 형식의 본문을작업하였음.
    2. image는 cloudinary 에서, text는 lorem ipsum 을 이용.

--- src/components/CenterBody.js
+++ src/components/CenterBody.js
@@ -1,16 +1,16 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Item from "./Item";
 
 const CenterBody = () => {
   return (
-    <Box
-      flex={3}
-      p={2}
-      sx={{
-        backgroundColor: "primary.main",
-      }}
-    >
-      CenterBody
+    <Box flex={3} p={2} sx={{}}>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
     </Box>
   );
 };
---
+++ src/components/Item.js
@@ -0,0 +1,58 @@
+import { Favorite, MoreVert, Share } from "@mui/icons-material";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const Item = () => {
+  return (
+    <Card>
+      <CardHeader
+        avatar={<Avatar sx={{ backgroundColor: "secondary.main" }}>R</Avatar>}
+        action={
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        }
+        title="Shrimp And Chorizo Paella"
+        subheader="May 27, 2024"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://res.cloudinary.com/jackie-cloudinary-app/image/upload/v1713699123/cld-sample-4.jpg"
+      />
+
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton>
+          <Favorite />
+        </IconButton>
+        <IconButton>
+          <Share />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Item;
